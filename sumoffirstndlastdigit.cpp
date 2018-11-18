aa1ca9fdd9e30b314527952386703a6384143a3e
masterishaan19 <45001655+masterishaan19@users.noreply.github.com>
2018-11-18 21:43:29 +0530

    sum of first and last digit of number

---
+++ sumoffirstndlastdigit.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int 
+	scanf("%d",&t);
+	int 
+	for(i=0;i<t;i++)
+	{
+		scanf("%d",&num);
+		int mat[num][num];
+		for(j=0;j<num;j++)
+		{
+			for(k=0;k<num;k++)
+			{
+				scanf("%d",mat[num][num])
+			}
+		}
+		
+	}
+
+}
