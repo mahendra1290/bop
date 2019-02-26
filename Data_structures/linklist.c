640b8dad12ece89a6301fdfc401d03d8e729f112
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-02-26 13:11:11 +0530

    fuction for creation and display

--- Data_structures/linklist.c
+++ Data_structures/linklist.c
@@ -6,44 +6,41 @@ typedef struct node{
 	struct node *ptr;
 }node;
 
-void displayLinklist(node *start, int len);
-node *createLinkList(int len);
+void displayLinklist(node *start);
+node *createLinkList();
 
 int main(){
 	int i, len, data;
 	printf("enter length of list\n");
 	scanf("%d", &len);
-    node *start = createLinkList(len);
-    displayLinklist(start, len+5);
+    node *start = createLinkList();
+    displayLinklist(start);
 }
 
-void displayLinklist(node *start, int len){
+void displayLinklist(node *start){
     node *temp = start;
-    int i=0;
-    do{
-        printf("data -> %d\n", temp->data);
+    printf("data in linklist...\n");
+    while(temp->data != -1){
+        printf("%d\n", temp->data);
         temp = temp->ptr;
     }
-    while(i++ < len);
 }
 
-node *createLinkList(int len){
-    int i=0, elem;
+node *createLinkList(){
+    int i=1, elem;
     node *temp;
     node *start = (node*)malloc(sizeof(node));
     node previous;
     previous.ptr = start;
-    printf("enter %d th data: ", i+1);
-    scanf("%d", &elem);
-    start->data = elem;
-    while(++i<len){
+    do{
+        printf("enter %d th data: ", i++);
+        scanf("%d", &elem);
+        (previous.ptr)->data = elem;
         temp = (node*)malloc(sizeof(node));
         (previous.ptr)->ptr = temp;
         previous.ptr = temp;
-        printf("enter %d th data: ", i+1);
-        scanf("%d", &elem);
-        temp->data = elem;
     }
+    while(elem != -1);
     temp->ptr = start;
     return start;
 }
