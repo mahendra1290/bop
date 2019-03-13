e120d3f7896c83a76578b28adb78dbb0bdbe750d
Mahendra <42772952+mahendra1290@users.noreply.github.com>
2019-03-14 00:59:08 +0530

    all alphabets and numbers added

--- matrix/matrix.ino
+++ matrix/matrix.ino
@@ -1,12 +1,66 @@
-void scrollString(char str[]);
+void scrollString(char str[], int speedd);
 void displayString(char str[], int showFor);
 void displayAlpha(char toPrint, int showFor);
+void displayPattern(int showFor);
+
 const byte rows[] = {1, 2, 3, 4, 5};
 const byte cols[] = {6, 7, 8, 9, 10};                
 byte bitSelect = 16;
 unsigned long tame;
-const char piyu[] = "mn";
-byte alphabets[27][5] = {{14, 10, 14, 10, 10}, //a
+const char piyu[] = "hello world ! ";
+
+byte pattern[50][5] = {{0, 0, 0, 0, 16},
+                        {0, 0, 0, 16, 16},
+                        {0, 0, 16, 16, 16},
+                        {0, 16, 16, 16, 16},
+                        {16, 16, 16, 16, 16},
+                        {24, 16, 16, 16, 16},
+                        {28, 16, 16, 16, 16},
+                        {30, 16, 16, 16, 16},
+                        {31, 16, 16, 16, 16},
+                        {31, 17, 16, 16, 16},
+                        {31, 17, 17, 16, 16},
+                        {31, 17, 17, 17, 16},
+                        {31, 17, 17, 17, 17},
+                        {31, 17, 17, 17, 19},
+                        {31, 17, 17, 17, 23},
+                        {31, 17, 17, 17, 31},
+                        {31, 17, 17, 17, 15},
+                        {31, 17, 17, 1, 15},
+                        {31, 17, 1, 1, 15},
+                        {31, 1, 1, 1, 15},
+                        {15, 1, 1, 1, 15},
+                        {7, 1, 1, 1, 15},
+                        {3, 1, 1, 1, 15},
+                        {1, 1, 1, 1, 15},
+                        {0, 1, 1, 1, 15},
+                        {0, 0, 1, 1, 15},
+                        {0, 0, 0, 1, 15},
+                        {0, 0, 0, 0, 15},
+                        {0, 0, 0, 0, 14},
+                        {0, 0, 0, 0, 12},
+                        {0, 0, 0, 0, 8},
+                        {0, 0, 0, 0, 0},
+                        {16, 0, 0, 0, 0},
+                        {24, 16, 0, 0, 0},
+                        {28, 24, 16, 0, 0},
+                        {30, 28, 24, 16, 0},
+                        {31, 30, 28, 24, 16},
+                        {31, 31, 30, 28, 24},
+                        {31, 31, 31, 30, 28},
+                        {31, 31, 31, 31, 30},
+                        {31, 31, 31, 31, 31},
+                        {15, 31, 31, 31, 31},
+                        {7, 15, 31, 31, 31},
+                        {3, 7, 15, 31, 31},
+                        {1, 3, 7, 15, 31},
+                        {0, 1, 3, 7, 15},
+                        {0, 0, 1, 3, 7},
+                        {0, 0, 0, 1, 3},
+                        {0, 0, 0, 0, 1},
+                        {0, 0, 0, 0, 0}
+                        };
+byte alphabets[39][5] = {{14, 10, 14, 10, 10}, //a
                          {14, 10, 14, 10, 14}, //b
                          {14, 8, 8, 8, 14},    //c
                          {14, 9, 9, 9, 14},    //d
@@ -32,7 +86,19 @@ byte alphabets[27][5] = {{14, 10, 14, 10, 10}, //a
                          {17, 10, 4, 10, 17},  //x
                          {10, 10, 4, 4, 4},    //y
                          {31, 2, 4, 8, 31},    //z
-                         {0, 0, 0, 0, 0},      //whitespace;
+                         {0, 0, 0, 0, 0},      //whitespace
+                         {4, 4, 4, 0, 4},      //exclamation
+                         {10, 21, 17 ,10 ,4},//heart
+                         {14, 10, 10, 10, 14},  //0
+                         {4, 4, 4, 4, 4}, //1
+                         {14, 2, 14, 8, 14}, //2
+                         {14, 2, 14, 2, 14}, //3
+                         {10, 10, 14, 2, 2},//4
+                         {14, 8, 14, 2, 14},//5
+                         {14, 8, 14, 10, 14},//6
+                         {14, 2, 2, 2, 2},//7
+                         {14, 10, 14, 10, 14},//8
+                         {14, 10, 14, 2, 14}//9
                         };
 void clearScreen(){
     for(int i=0; i<5; i++){
@@ -53,7 +119,7 @@ void setup() {
 }
   
 void loop() {
-    scrollString(piyu);
+    scrollString(piyu, 200);
 }
 
 void displayString(char str[], int showFor){
@@ -66,12 +132,15 @@ void displayString(char str[], int showFor){
 
 void displayAlpha(char toPrint, int showFor){
     int alpha;
-    if(toPrint == 32){
-        alpha = 26;
-    }
-    else{
-        alpha = (toPrint-97);
-    }
+     if(toPrint >= 48 && toPrint <= 57){
+            alpha = 29+(toPrint-48);
+        }
+        else if(toPrint == 32 || toPrint == 33 || toPrint == 34){
+            alpha = 26+(toPrint-32);
+        }
+        else{
+            alpha = toPrint-97;
+        }
     tame = millis();
     while(millis()-tame < showFor){
         for(int i=0; i<5; i++){
@@ -92,9 +161,9 @@ void displayAlpha(char toPrint, int showFor){
 }
 
 
-void displayMat(byte mat[]){
+void displayMat(byte mat[], int showFor){
     tame = millis();
-        while(millis()-tame < 1000){
+        while(millis()-tame < showFor){
         for(int i=0; i<5; i++){
         digitalWrite(rows[i], LOW);
         bitSelect = 16;
@@ -112,30 +181,55 @@ void displayMat(byte mat[]){
     }
 }
 
-void scrollString(char str[]){
+void scrollString(char str[], int speedd){
     byte current[5];
     byte next[5];
     byte tomap = B00100000;
     int i=0;
     int k=0;
+    int curr, nxt;
     while(str[k+1] != NULL){
+        if(str[k] >= 48 && str[k] <= 57){
+            curr = 29+(str[k]-48);
+        }
+        else if(str[k] == 32 || str[k] == 33|| str[k] == 34){
+            curr = 26+(str[k]-32);
+        }
+        else{
+            curr = str[k]-97;
+        }
+        if(str[k+1] >= 48 && str[k+1] <= 57){
+            nxt = 29+(str[k+1]-48);
+        }
+        else if(str[k+1] == 32 || str[k+1] == 33 || str[k+1] == 34){
+            nxt = 26+(str[k+1]-32);
+        }
+        else{
+            nxt = str[k+1]-97;
+        }
         tomap = B00100000;
         for(i=0; i<5; i++){
-            current[i] = alphabets[str[k]-97][i];
-            next[i]    = alphabets[str[k+1]-97][i];
+            current[i] = alphabets[curr][i];
+            next[i]    = alphabets[nxt][i];
         }
-        for(int j=0; j<6; j++){
+        for(int j=0; j<5; j++){
             for(i=0; i<5; i++){
                 if(tomap & next[i]){
                     current[i] = (current[i] | 1);
                 }
             }
-            displayMat(current);
+            displayMat(current, speedd);
             tomap >>= 1;
             for(i=0; i<5; i++){
                 current[i] <<= 1;
             }
         }
         k++;
+    }
+}
+
+void displayPattern(int showFor){
+    for(int i=0; i<50; i++){
+        displayMat(pattern[i], showFor);
     }
 }
