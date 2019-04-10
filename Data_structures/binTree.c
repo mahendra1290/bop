76aaf0beb00e9ed3950d2d764e5fc0bdfcc39947
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-04-10 12:36:21 +0530

    binary tree delete added

--- Data_structures/binTree.c
+++ Data_structures/binTree.c
@@ -2,38 +2,73 @@
 #include <stdlib.h>
 #include <limits.h>
 
-typedef struct binTreeNode *binTreePointer;
+#define MALLOC(ptr, size) \
+    if(!((ptr) = malloc(size))){\
+        fprintf(stderr, "Insufficient memory");\
+        exit(EXIT_FAILURE);\
+    }
 
-typedef struct binTreeNode{
+#define NODE_SIZE (sizeof(treeNode))
+
+typedef struct treeNode *treePointer;
+
+typedef struct treeNode{
     int data;
-    binTreePointer leftChild;
-    binTreePointer rightChild;
-}binTreeNode;
+    treePointer left;
+    treePointer right;
+    treePointer parent;
+}treeNode;
 
-binTreePointer createBinTree(binTreePointer parent, char child);
-void inorderTraversal(binTreePointer root);
-void preorderTraversal(binTreePointer root);
-void postorderTraversal(binTreePointer root);
-int findMaxInBinTree(binTreePointer root);
-int getSumOfBinTree(binTreePointer root);
-int getDepthOfBinTree(binTreePointer root);
-int getLeafNodeCount(binTreePointer root);
-int getTotalNodeCount(binTreePointer root);
-int getNonLeafNodeCount(binTreePointer root);
-binTreePointer searchInBinTree(binTreePointer root, int value);
-void binTreeOper(binTreePointer root);
+treePointer createBinTree(treePointer parent, char child);
+void inorderTraversal(treePointer root);
+void preorderTraversal(treePointer root);
+void postorderTraversal(treePointer root);
+int findMaxInBinTree(treePointer root);
+int getSumOfBinTree(treePointer root);
+int getDepthOfBinTree(treePointer root);
+int getLeafNodeCount(treePointer root);
+int getTotalNodeCount(treePointer root);
+int getNonLeafNodeCount(treePointer root);
+treePointer searchInBinTree(treePointer root, int value);
+void binTreeOper(treePointer root);
+treePointer insertion(treePointer root, int value);
+treePointer createBinSearchTree(treePointer root, int val);
+treePointer searchInBst(treePointer root, int val);
+void deleteNode(treePointer node);
+treePointer getSuccessor(treePointer node);
+treePointer getPreccessor(treePointer node);
 
 int main(){
-    binTreePointer root = NULL;
-    binTreeOper(root);
+    treePointer root = NULL;
+    root = createBinSearchTree(root, 10);
+    inorderTraversal(root);
+    treePointer temp;
+    temp = searchInBinTree(root, 5);
+    deleteNode(temp);
+    inorderTraversal(root);
+}
+//create binary search tree
+treePointer createBinSearchTree(treePointer root, int noOfItems){
+    if(noOfItems == 0){
+        return root;
+    }
+    int value;
+    scanf("%d", &value);
+    if(root == NULL){
+        root = insertion(root, value);
+    }
+    else{
+        insertion(root, value);
+    }
+    return createBinSearchTree(root, noOfItems-1);
 }
 //--------------------------------------------------------------
-binTreePointer createBinTree(binTreePointer parent, char child){
-    binTreePointer root = NULL;
+treePointer createBinTree(treePointer parent, char child){
+    treePointer root = NULL;
     int choice;
-    root = (binTreePointer)malloc(sizeof(binTreeNode));
-    root->leftChild  = NULL;
-    root->rightChild = NULL;
+    root = (treePointer)malloc(sizeof(treeNode));
+    root->left  = NULL;
+    root->right = NULL;
     if(child == 'n'){
         printf("enter data of root : ");
         scanf("%d", &(root->data));
@@ -49,50 +84,50 @@ binTreePointer createBinTree(binTreePointer parent, char child){
     printf("\ndoes left child exist for data %d [1|0]..? ", root->data);
     scanf(" %d", &choice);
     if(choice == 1){
-        root->leftChild = createBinTree(root, 'l');
+        root->left = createBinTree(root, 'l');
     }
     printf("\ndoes right child exist for data %d [1|0]..? ", root->data);
     scanf(" %d", &choice);
     if(choice == 1){
-        root->rightChild = createBinTree(root, 'r');
+        root->right = createBinTree(root, 'r');
     }
     return root;
 }
 //--------------------------------------------
-void inorderTraversal(binTreePointer root){
+void inorderTraversal(treePointer root){
     if(root == NULL){
         return;
     }
-    inorderTraversal(root->leftChild);
+    inorderTraversal(root->left);
     printf("%d ", root->data);
-    inorderTraversal(root->rightChild);
+    inorderTraversal(root->right);
 }
 //---------------------------------------------
-void preorderTraversal(binTreePointer root){
+void preorderTraversal(treePointer root){
     if(root == NULL){
         return;
     }
     printf("%d ", root->data);
-    preorderTraversal(root->leftChild);
-    preorderTraversal(root->rightChild);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
 }
 //---------------------------------------------
-void postorderTraversal(binTreePointer root){
+void postorderTraversal(treePointer root){
     if(root == NULL){
         return;
     }
-    postorderTraversal(root->leftChild);
-    postorderTraversal(root->rightChild);
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
     printf("%d ", root->data);
 }
 //----------------------------------------------
-int findMaxInBinTree(binTreePointer root){
+int findMaxInBinTree(treePointer root){
     int x, y, z;
     if(root == NULL){
         return INT_MIN;
     }
-    x = findMaxInBinTree(root->leftChild);
-    y = findMaxInBinTree(root->rightChild);
+    x = findMaxInBinTree(root->left);
+    y = findMaxInBinTree(root->right);
     z = root->data;
     if(x > y && x > z)
         return x;
@@ -102,72 +137,72 @@ int findMaxInBinTree(binTreePointer root){
         return z;
 }
 //---------------------------------------------
-int getSumOfBinTree(binTreePointer root){
+int getSumOfBinTree(treePointer root){
     if(root == NULL){
         return 0;
     }
-    return root->data + getSumOfBinTree(root->leftChild) + getSumOfBinTree(root->rightChild);
+    return root->data + getSumOfBinTree(root->left) + getSumOfBinTree(root->right);
 }
 //-------------------------------------------------
-int getDepthOfBinTree(binTreePointer root){
+int getDepthOfBinTree(treePointer root){
     int leftChildDepth;
     int rightChildDepth;
     if(root == NULL){
         return 0;
     }
-    leftChildDepth  = getDepthOfBinTree(root->leftChild);
-    rightChildDepth = getDepthOfBinTree(root->rightChild);
+    leftChildDepth  = getDepthOfBinTree(root->left);
+    rightChildDepth = getDepthOfBinTree(root->right);
     if(leftChildDepth > rightChildDepth){
         return (1+leftChildDepth);
     }
     return (1+rightChildDepth);
 }
 //--------------------------------------------------
-int getTotalNodeCount(binTreePointer root){
+int getTotalNodeCount(treePointer root){
     if(root == NULL){
         return 0;
     }
-    return (1+getTotalNodeCount(root->leftChild)+getTotalNodeCount(root->rightChild));
+    return (1+getTotalNodeCount(root->left)+getTotalNodeCount(root->right));
 }
 //-------------------------------------------------------------------
-int getLeafNodeCount(binTreePointer root){
+int getLeafNodeCount(treePointer root){
     if(root == NULL){
         return 0;
     }
-    else if(root->leftChild == NULL && root->rightChild == NULL){
+    else if(root->left == NULL && root->right == NULL){
         return 1;
     }
-    return getLeafNodeCount(root->leftChild)+getLeafNodeCount(root->rightChild);
+    return getLeafNodeCount(root->left)+getLeafNodeCount(root->right);
 }
 //------------------------------------------------------------------
-int getNonLeafNodeCount(binTreePointer root){
+int getNonLeafNodeCount(treePointer root){
     if(root == NULL){
         return 0;
     }
-    else if(root->leftChild != NULL || root->rightChild != NULL){
+    else if(root->left != NULL || root->right != NULL){
         return 1;
     }
-    return getNonLeafNodeCount(root->leftChild)+getNonLeafNodeCount(root->rightChild);
+    return getNonLeafNodeCount(root->left)+getNonLeafNodeCount(root->right);
 }
 //------------------------------------------------------------------
-binTreePointer searchInBinTree(binTreePointer root, int value){
-    binTreePointer leftSearch  = NULL;
-    binTreePointer rightSearch = NULL;
+treePointer searchInBinTree(treePointer root, int value){
+    treePointer leftSearch  = NULL;
+    treePointer rightSearch = NULL;
     if(root == NULL){
         return NULL;
     }
     else if(root->data == value){
         return root;
     }
-    leftSearch = searchInBinTree(root->leftChild, value);
+    leftSearch = searchInBinTree(root->left, value);
     if(leftSearch == NULL){
-        rightSearch = searchInBinTree(root->rightChild, value);
+        rightSearch = searchInBinTree(root->right, value);
         return rightSearch;
     }
     return leftSearch;
 }
 //---------------------------------------------------------
-void binTreeOper(binTreePointer root){
+void binTreeOper(treePointer root){
     int choice;
     int toSearch;
     int conti;
@@ -233,4 +268,140 @@ void binTreeOper(binTreePointer root){
         scanf("%d", &conti);
     }
     while(conti == 1);
+}
+
+treePointer insertion(treePointer root, int value){
+    if(root==NULL){
+        MALLOC(root, NODE_SIZE);
+        root->data = value;
+        root->left = NULL;
+        root->right = NULL;
+        root->parent = NULL;
+        return root;
+    }
+    else{
+        if(value > root->data){
+            if(root->right != NULL){
+                insertion(root->right, value);
+            }
+            else{
+                treePointer newNode = NULL;
+                MALLOC(newNode, NODE_SIZE);
+                newNode->data = value;
+                newNode->right = NULL;
+                newNode->left = NULL;
+                newNode->parent = root;
+                root->right = newNode;
+            }
+        }
+        else{
+            if(root->left != NULL){
+                insertion(root->left, value);
+            }
+            else{
+                treePointer newNode = NULL;
+                MALLOC(newNode, NODE_SIZE);
+                newNode->data = value;
+                newNode->right = NULL;
+                newNode->left = NULL;
+                newNode->parent = root;
+                root->left = newNode;
+            }
+        }
+    }
+    return root;
+}
+//search function for bst
+treePointer searchInBst(treePointer root, int val){
+    if(root == NULL || root->data == val)
+        return root;
+    if(val > root->data)
+        return searchInBinTree(root->right, val);
+    return searchInBinTree(root->left, val);
+}
+//get successor of a node
+treePointer getSuccessor(treePointer node){
+    treePointer y = NULL;
+    treePointer parent = NULL;
+    if(node == NULL)
+        return node;
+    y = node->right;
+    if(y == NULL){
+        parent = node->parent;
+        while(parent != NULL && node != NULL){
+            if(parent->left == node){
+                break;
+            }
+            node = node->parent;
+        }
+        return parent;
+    }
+    while(y->left != NULL){
+        y = y->left;
+    }
+    return y;
+}
+//get preccessor of node
+treePointer getPreccessor(treePointer node){
+    treePointer y = NULL;
+    treePointer parent = NULL;
+    if(node == NULL)
+        return node;
+    y = node->left;
+    if(y == NULL){
+        parent = node->parent;
+        while(parent != NULL && node != NULL){
+            if(parent->right == node){
+                break;
+            }
+            node = node->parent;
+        }
+        return parent;
+    }
+    while(y->right != NULL){
+        y = y->right;
+    }
+    return y;
+}
+
+void deleteNode(treePointer node){
+    if(node){
+        if(node->left == NULL && node->right == NULL){
+            treePointer parent = node->parent;
+            if(parent){
+                if(parent->left == node)
+                    parent->left = NULL;
+                else
+                    parent->right = NULL;
+            }
+            free(node);
+        }
+        else if(node->left == NULL || node->right == NULL){
+            treePointer parent = node->parent;
+            if(parent && node->left){
+                if(node == parent->left){
+                    parent->left = node->left;
+                }
+                else{
+                    parent->right = node->left;
+                }
+                (node->left)->parent = parent;
+            }
+            else if(parent && node->right){
+                if(node == parent->left){
+                    parent->left = node->right;
+                }
+                else{
+                    parent->right = node->right;
+                }
+                (node->right)->parent = parent;
+            }
+            free(node);
+        }
+        else{
+            treePointer successor = getSuccessor(node);
+            node->data = successor->data;
+            deleteNode(successor);
+        }
+    }
 }
