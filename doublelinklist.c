b1d5db1da17acb35c1afc3ab947818739dee472c
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-04-15 00:45:24 +0530

    changes made

--- doublelinklist.c
+++ doublelinklist.c
@@ -1,356 +1,308 @@
-/*
-all function of double linklist
-created : 03/03/19
-author  : mahendra
-*/ 
-
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct node{
-    int data;
-    struct node *back;
-    struct node *next;
-}node;
-
-node *createDoubLinklist();
-void traverse(node *start);
-void traverseReverse(node *start);
-node *insertAtPosi(node *start, int position, int newValue);
-void insertAfter(node *start, int value, int newValue);
-node *insertBefore(node *start, int value, int newValue);
-void deleteAfter(node *start, int value);
-node *deleteNode(node *start, int value);
-node *deleteNodeAtPos(node *start, int position);
-
-int main()
+typedef struct node
 {
-    node *start;
-    start = createDoubLinklist();
-    traverse(start);
-    deleteAfter(start, 39);
-    traverse(start);
-    traverseReverse(start);
+    int data;
+    struct node* fptr;
+    struct node* bptr;
+
+}node;
+node * deleteElemBeforeVal(node *start, int val);
+void deleteElemAfterVal(node *start, int val);
+node *deleteElem(node *start,int elem);
+node *deleteAtPos(node* start,int pos);
+node *InsertBeforeElem(node *start, int elem,int toinsert);
+node *InsertAtPos(node*start, int pos, int toinsert);
+void InsertAfterElem(node*start, int elem, int toinsert);
+void displayBack(node *start);
+void display(node *start);
+node * createDLL();
+node * createDLL()
+{
+    int  val;
+    node *start = (node*)malloc(sizeof(node));
+    node *temp=start, *reqptr = NULL;
+    start->bptr = NULL;
+    printf("Input will be taken untill -1 is entered...\n");
+    while(val!=-1)
+    {
+        temp->bptr = reqptr;
+        printf("Enter the value :");
+        scanf("%d",&val);
+        node *new_node = (node*)malloc(sizeof(node));
+        temp->data = val;
+        temp->fptr = new_node;
+        reqptr = temp;
+        temp = new_node;
+    }
+    reqptr->bptr->fptr = NULL;
+    return start;
+
 }
-//-------------------------------------------------------
-node *createDoubLinklist(){
-    int i=1, elem;
-    int mode  = 1;
-    int count = 0;
-    char conti = 'y';
-    node *temp;
-    node *prevNode;
-    node *start;
-    node *newNode;
-    start = (node*)malloc(sizeof(node));
-    if(start == NULL){
-        printf("ERROR: memory can't be allocated\n");
+
+void displayBack(node *start)
+{
+    node *temp= start;
+    while(temp->fptr!=NULL)
+    {
+        temp = temp->fptr;
+    }
+    while(temp!=NULL)
+    {
+        printf("\nval =%d ..bptr = %d.. fptr = %d\n",temp->data, temp->bptr,temp->fptr);
+        temp = temp->bptr;
+    }
+}
+
+void display(node *start)
+{
+    node*temp = start;
+    do
+    {
+        printf("\nval =%d ..bptr = %d.. fptr = %d\n",temp->data, temp->bptr,temp->fptr);
+        temp = temp->fptr;
+    }
+    while(temp!=0);
+}
+
+void InsertAfterElem(node*start, int elem, int toinsert)
+{
+    node *temp = start;
+    while(temp->data!=elem)
+    {
+        temp = temp->fptr;
+    }
+    node *new_node = (node*)malloc(sizeof(node));
+    new_node->bptr = temp;
+    new_node->data = toinsert;
+    new_node->fptr = temp->fptr;
+    if(temp->fptr==NULL)
+    {
+        temp->fptr = new_node;
+        return;
+    }
+    temp->fptr->bptr = new_node;
+    temp->fptr = new_node;
+}
+node *InsertAtPos(node*start, int pos, int toinsert)
+{
+    node *temp=start;
+    int temp_pos = pos-2;
+    node *new_node = (node*)malloc(sizeof(node));
+    if(pos==1)
+    {
+        new_node->bptr = NULL;
+        new_node->data = toinsert;
+        new_node->fptr = start;
+        start->bptr = new_node;
+        return new_node;
+    }
+    else
+    {
+        while(temp_pos--)
+        {
+            temp = temp->fptr;
+        }
+        new_node->data = toinsert;
+        new_node->bptr = temp;
+        new_node->fptr = temp->fptr;
+        if(temp->fptr==NULL)
+        {
+            temp->fptr = new_node;
+            return start;
+        }
+        temp->fptr->bptr = new_node;
+        temp->fptr = new_node;
         return start;
     }
-    else{
-        temp = start;
-        prevNode  = temp;
-        printf("enter data of first node: ");
-        scanf("%d", &elem);
-        temp->data  = elem;
-        printf("select mode to enter data\n");
-        printf("[1] enter fixed number of data\n");
-        printf("[2] enter data till you want\n");
-        printf("[3] create no new node\n");
-        scanf("%d",&mode);
-        switch(mode){
-        case 1:
-            printf("enter number of data to enter: ");
-            scanf("%d", &count);
-            i = 1;
-            while(i <= count){
-                newNode = (node *)malloc(sizeof(node));
-                if(newNode == NULL){
-                    printf("ERROR: memory can't be allocated\n");
-                    return start;
-                }
-                else{
-                    printf("enter %d th data: ", i);
-                    scanf("%d", &elem);
-                    newNode->data = elem;
-                    prevNode->next = newNode;
-                    newNode->back = prevNode;
-                    prevNode = newNode;
-                    i++;
-                }
-            }
-            break;
-        case 2:
-            printf("want to add more node? [y/n] ");
-            scanf(" %c", &conti);
-            while(conti == 'Y' || conti == 'y'){
-                newNode = (node *)malloc(sizeof(node));
-                if(newNode == NULL){
-                    printf("ERROR: memory can't be allocated\n");
-                    return start;
-                }
-                printf("enter data: ");
-                scanf("%d", &elem);
-                newNode->data = elem;
-                prevNode->next = newNode;
-                newNode->back = prevNode;
-                prevNode = newNode;
-                printf("want to add more node? [y/n] ");
-                scanf(" %c", &conti);
-            }
-            break;
-            
-        default:
-            printf("no new node added\n");                             
-            break;
+}
+node *InsertBeforeElem(node *start, int elem,int toinsert)
+{
+    node *temp = start;
+    node *new_node = (node*)malloc(sizeof(node));
+    while(temp->data!=elem)
+    {
+        temp = temp->fptr;
+    }
+    if(temp==start)
+    {
+        new_node->data = toinsert;
+        new_node->fptr=start;
+        start->bptr = new_node;
+        new_node->bptr = NULL;
+    }
+    else
+    {
+        new_node->data = toinsert;
+        new_node->fptr = temp;
+        new_node->bptr = temp->bptr;
+        (temp->bptr)->fptr = new_node;
+        temp->bptr = new_node;
+        return start;
+    }
+}
+
+node *deleteAtPos(node* start,int pos)
+{
+    node *temp =start;
+    int temp_pos = pos-1;
+    if(pos==1)
+    {
+        temp->fptr->bptr=NULL;
+        return temp->fptr;
+    }
+    else
+    {
+        while(temp_pos--)
+        {
+            temp = temp->fptr;
         }
+        if(temp->fptr==NULL)
+        {
+            temp->bptr->fptr = NULL;
+        }
+        else
+        {
+            temp->bptr->fptr = temp->fptr;
+            temp->fptr->bptr = temp->bptr;
+        }
+        return start;
+    }
+}
+node *deleteElem(node *start,int elem)
+{
+    node* temp = start;
+    while(temp->data!=elem)
+    {
+        temp = temp->fptr;
+    }
+    if(temp==start)
+    {
+        temp->fptr->bptr = NULL;
+        return temp->fptr;
+    }
+    else if(temp->fptr==NULL)
+    {
+        temp->bptr->fptr=NULL;
+    }
+    else
+    {
+        temp->bptr->fptr = temp->fptr;
+        temp->fptr->bptr = temp->bptr;
     }
     return start;
 }
-//-------------------------------------------------------
-void traverse(node *start){
+
+void deleteElemAfterVal(node *start, int val)
+{
     node *temp = start;
-    if(temp == NULL){
-        printf("list is empty\n");
-        return;
+    while(temp->data!=val)
+    {
+        temp = temp->fptr;
     }
-    else{
-        while(temp != NULL){
-            printf("%d ", temp->data);
-            temp = temp->next;
-        }
-        printf("\n");
-    }   
-}
-//-------------------------------------------------------
-void traverseReverse(node *start){
-    node *temp = start;
-    if(temp == NULL){
-        printf("list is empty\n");
-        return;
+    if((temp->fptr)->fptr==NULL)
+    {
+        temp->fptr= NULL;
+
     }
-    else{
-        while(temp->next != NULL){
-            temp = temp->next;
-        }
-        while(temp != NULL){
-            printf("%d ", temp->data);
-            temp = temp->back;
-        }
-        printf("\n");
-    }
-}
-//-------------------------------------------------------
-node *insertAtPosi(node *start, int position, int newValue){
-    int i = 1;
-    node *newStart = NULL;
-    node *temp = NULL;
-    node *newNode = NULL;
-    node *prevNode = NULL;
-    newStart = start;
-    temp = start;
-    if(temp == NULL){
-        printf("list is empty\n");
-        return newStart;
-    }
-    i = 1;
-    while(temp->next != NULL && i < position){
-        temp = temp->next;
-        i++;
-    }
-    if(temp->next == NULL && position > i){
-        printf("POSITION ERROR\n");
-        printf("insertion operation failed\n");
-        return newStart;
-    }
-    newNode = (node *)malloc(sizeof(node));
-    newNode->data = newValue;
-    
-    if(position == 1){
-        newNode->back = NULL;
-        newNode->next = temp;
-        temp->back = newNode;
-        newStart = newNode;
-    }
-    else{
-        prevNode = temp->back;
-        prevNode->next = newNode;
-        newNode->back  = prevNode;
-        newNode->next  = temp;
-        temp->back     = newNode;
-    }
-    return newStart;
-}
-//-------------------------------------------------------
-void insertAfter(node *start, int value, int newValue){
-    node *temp = NULL;
-    node *newNode = NULL;
-    temp = start;
-    if(temp == NULL){
-        printf("list is empty\n");
-        return;
-    }
-    while(temp->next != NULL && temp->data != value){
-        temp = temp->next;
-    }
-    if(temp->next == NULL && temp->data != value){
-        printf("VALUE ERROR\n");
-        printf("insertion operation failed\n");
-        return;
-    }
-    newNode = (node *)malloc(sizeof(node));
-    newNode->data = newValue;
-    newNode->next = temp->next;
-    newNode->back = temp;
-    (temp->next)->back = newNode;
-    temp->next = newNode;
-}
-//-------------------------------------------------------
-node *insertBefore(node *start, int value, int newValue){
-    node *temp = NULL;
-    node *newStart = NULL;
-    node *newNode = NULL;
-    temp = start;
-    newStart = start;
-    if(temp == NULL){
-        printf("list is empty\n");
-        return newStart;
-    }
-    while(temp->next != NULL && temp->data != value){
-        temp = temp->next;
-    }
-    if(temp->next == NULL && temp->data != value){
-        printf("VALUE ERROR\n");
-        printf("insertion operation failed\n");
-        return newStart; 
-    }
-    newNode = (node *)malloc(sizeof(node));
-    newNode->data = newValue;
-    newNode->next = temp;
-    newNode->back = (temp->back);
-    if(temp == start)
-        newStart = newNode;
     else
-        (temp->back)->next = newNode;
-    temp->back = newNode;
-    return newStart;
+    {
+        temp->fptr = temp->fptr->fptr;
+        temp->fptr->bptr = temp;
+    }
 }
-//-------------------------------------------------------
-node *deleteNodeAtPos(node *start, int position){
-    int i = 1;
-    node *newStart = NULL;
-    node *temp = NULL;
-    node *newNode = NULL;
-    node *prevNode = NULL;
-    node *toDelete = NULL;
-    newStart = start;
-    temp = start;
-    if(temp == NULL){
-        printf("list is empty\n");
-        return newStart;
+node * deleteElemBeforeVal(node *start, int val)
+{
+    node *temp = start;
+    while(temp->data!=val)
+    {
+        temp  = temp->fptr;
     }
-    i = 1;
-    while(temp->next != NULL && i < position){
-        temp = temp->next;
-        i++;
+    if(temp->bptr==start)
+    {
+        temp->bptr = NULL;
+        free(start);
+        return temp;
+
     }
-    if(temp->next == NULL && position > i){
-        printf("POSITION ERROR\n");
-        printf("insertion operation failed\n");
-        return newStart;
+    else
+    {
+        node *temp_ptr=temp->bptr;
+        temp->bptr = (temp_ptr)->bptr;
+        (temp_ptr)->fptr = temp;
+        free(temp_ptr);
+        return start;
     }
-    toDelete = temp;
-    if(position == 1 && temp->next != NULL){
-        newStart = temp->next;
-        (temp->next)->back = NULL;
-    }
-    else if(position == 1 && temp->next == NULL){
-        newStart = NULL;
-    }
-    else{
-        prevNode = temp->back;
-        if(temp->next != NULL){
-            prevNode->next = temp->next;
-            (temp->next)->back = prevNode;
-        }
-        else{
-            prevNode->next = NULL;
-        }
-    }
-    free(toDelete);
-    return newStart;
+
+
 }
-//----------------------------------------------------
-node *deleteNode(node *start, int value){
-    node *newStart = NULL;
-    node *temp = NULL;
-    node *newNode = NULL;
-    node *prevNode = NULL;
-    node *toDelete = NULL;
-    newStart = start;
-    temp = start;
-    if(temp == NULL){
-        printf("list is empty\n");
-        return newStart;
-    }
-    while(temp->next != NULL && temp->data != value){
-        temp = temp->next;
-    }
-    if(temp->next == NULL && temp->data != value){
-        printf("VALUE ERROR\n");
-        printf("insertion operation failed\n");
-        return newStart;
-    }
-    toDelete = temp;
-    if(temp == start && temp->next != NULL){
-        newStart = temp->next;
-        (temp->next)->back = NULL;
-    }
-    else if(temp == start && temp->next == NULL){
-        newStart = NULL;
-    }
-    else{
-        prevNode = temp->back;
-        if(temp->next != NULL){
-            prevNode->next = temp->next;
-            (temp->next)->back = prevNode;
+int main()
+{
+    node *start = createDLL();
+    int check =1;
+    int elem, toinsert,pos;
+    while(check)
+    {
+        printf("Enter the operation\n");
+        printf("1 : Insert before an Element\n");
+        printf("2 : Insert at a particular position\n");
+        printf("3: Insert after an element\n");
+        printf("4: Delete an element\n");
+        printf("5: Delete after an element\n");
+        printf("6: Delete before an element\n");
+        printf("7: Delete at a position\n");
+        printf("8: Display from front\n");
+        printf("9: Display from back\n");
+        printf("10 : Exit");
+        int ope;
+        scanf("%d",&ope);
+
+        switch(ope)
+        {
+            case 1:
+                printf("Enter the element before you want to insert and the value you want to insert\n");
+                scanf("%d%d",&elem,&toinsert);
+                start = InsertBeforeElem(start, elem,toinsert);
+                break;
+            case 2:
+                printf("Enter the position you want to insert and the value you want to insert\n");
+                scanf("%d%d",&pos,&toinsert);
+                start = InsertAtPos(start, pos, toinsert);
+                break;
+            case 3:
+                printf("Enter the element after you want to insert and the value you want to insert\n");
+                scanf("%d%d",&elem,&toinsert);
+                InsertAfterElem(start, elem,toinsert);
+                break;
+            case 4:
+                printf("Enter the element you want to delete\n");
+                scanf("%d",&elem);
+                start = deleteElem(start,elem);
+                break;
+            case 5:
+                printf("Enter the element after you want to delete\n");
+                scanf("%d",&elem);
+                deleteElemAfterVal(start, elem);
+                break;
+            case 6:
+                printf("Enter the before after you want to delete\n");
+                scanf("%d",&elem);
+                start = deleteElemBeforeVal(start, elem);
+                break;
+            case 7:
+                printf("Enter the position you want to delete\n");
+                scanf("%d",&pos);
+                start = deleteAtPos(start, pos);
+                break;
+            case 8:
+                display(start);
+                break;
+            case 9:
+                displayBack(start);
+                break;
+            case 10:
+                check =0;
         }
-        else{
-            prevNode->next = NULL;
-        }
-    }
-    free(toDelete);
-    return newStart;
-}
-//------------------------------------------------------------
-void deleteAfter(node *start, int value){
-    node *temp = NULL;
-    node *newNode = NULL;
-    temp = start;
-    node *toDelete = NULL;
-    if(temp == NULL){
-        printf("list is empty\n");
-        return;
-    }
-    while(temp->next != NULL && temp->data != value){
-        temp = temp->next;
-    }
-    if(temp->next == NULL && temp->data != value){
-        printf("VALUE ERROR\n");
-        printf("insertion operation failed\n");
-        return;
-    }
-    if(temp->next != NULL){
-        toDelete = temp->next;
-        if((temp->next)->next != NULL){
-            temp->next = (temp->next)->next;
-            (temp->next)->back = temp;
-        }
-        else
-            temp->next = NULL;
-        free(toDelete);   
-    }
-    else{
-        printf("next node does't exist\n");
     }
 }
